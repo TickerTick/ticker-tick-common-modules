bae9318582285ebca2371d118ea5aeae5c15b30d
yumindev <yumindev@gmail.com>
2021-12-31 15:30:23 -0800

    fix: 0.0.11

--- packages/helpers/webpack.config.js
+++ packages/helpers/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: "./lib/index.js",
+  entry: ["regenerator-runtime/runtime.js", "./lib/index.js"],
   output: {
     path: path.resolve("dist"),
     filename: "index.js",
